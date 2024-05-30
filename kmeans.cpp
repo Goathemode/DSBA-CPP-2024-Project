8ada564fe638ae21536daa8eb4afb204451eabd8
sevadelorean12 <51268729+sevadelorean12@users.noreply.github.com>
2024-05-30 03:02:09 +0300

    Update kmeans.cpp

--- kmeans.cpp
+++ kmeans.cpp
@@ -8,6 +8,7 @@
 #include <algorithm>
 #include <random>
 #include <limits>
+#include <numeric>
 
 
 using namespace std;
@@ -21,7 +22,7 @@ class DynamicClass {
             fields[name] = value;
         }
 
-        double getField(string name) {
+        double getField(string name) const {
             if (fields.find(name) != fields.end()) {
                 return fields.at(name);
             } else {
@@ -31,7 +32,7 @@ class DynamicClass {
 };
 
 // Отсеивает целые и дробные числа, затесавшиеся в string (т.к клетка файла .csv - string)
-bool isNumber(string str1) {
+bool isNumber(const string& str1) {
     bool decimalPointFound = false;
     bool negativeSignFound = false;
     int charCount = 0;
@@ -50,19 +51,19 @@ bool isNumber(string str1) {
     return true;
 }
 
-// Эвклидово расстояние
-double euclideanDistance(vector<string> field_names1, DynamicClass obj1, DynamicClass obj2) {
-    double sum1 = 0.0;
-    for (size_t i = 0; i < field_names1.size(); i++) {
-        double diff = obj1.getField(field_names1[i]) - obj2.getField(field_names1[i]);
-        sum1 += diff * diff;
+
+double euclideanDistance(const vector<string>& field_names, const DynamicClass& obj1, const DynamicClass& obj2) {
+    double sum = 0.0;
+    for (const auto& field_name : field_names) {
+        double diff = obj1.getField(field_name) - obj2.getField(field_name);
+        sum += diff * diff;
     }
-    return sqrt(sum1);
+    return sqrt(sum);
 }
 
-vector<DynamicClass> initializeCentroids(vector<DynamicClass>& data2, int k) {
+vector<DynamicClass> initializeCentroids(const vector<DynamicClass>& data, int k) {
     vector<DynamicClass> centroids;
-    vector<DynamicClass> shuffledData = data2;
+    vector<DynamicClass> shuffledData = data;
     random_device rd;
     default_random_engine rng(rd()); // бог знает как это работает
     shuffle(shuffledData.begin(), shuffledData.end(), rng);
@@ -72,123 +73,165 @@ vector<DynamicClass> initializeCentroids(vector<DynamicClass>& data2, int k) {
     return centroids;
 }
 
-vector<int> assignClusters(vector<string> field_names1, vector<DynamicClass>& data, vector<DynamicClass> centroids1) {
-    vector<int> clusters;
-    for (DynamicClass point : data) {
+vector<int> assignClusters(const vector<string>& field_names, const vector<DynamicClass>& data, const vector<DynamicClass>& centroids) {
+    vector<int> clusters(data.size());
+    for (size_t i = 0; i < data.size(); ++i) {
         double minDist = numeric_limits<double>::max();
         int closestCentroid = -1;
-        for (size_t i = 0; i < centroids1.size(); i++) {
-            double dist = euclideanDistance(field_names1, point, centroids1[i]);
+        for (size_t j = 0; j < centroids.size(); ++j) {
+            double dist = euclideanDistance(field_names, data[i], centroids[j]);
             if (dist < minDist) {
                 minDist = dist;
-                closestCentroid = i;
+                closestCentroid = j;
             }
         }
-        clusters.push_back(closestCentroid);
+        clusters[i] = closestCentroid;
     }
     return clusters;
 }
 
-vector<DynamicClass> updateCentroids(vector<DynamicClass>& data, vector<int> clusters, int k) {
+vector<DynamicClass> updateCentroids(const vector<DynamicClass>& data, const vector<int>& clusters, int k) {
     vector<DynamicClass> newCentroids(k);
     vector<int> clusterCounts(k, 0);
-    for (int i = 0; i < k; i++) {
-        newCentroids[i].fields.clear();
-    }
-    for (size_t i = 0; i < data.size(); i++) {
+    for (size_t i = 0; i < data.size(); ++i) {
         int cluster = clusters[i];
-        for (auto [name, value] : data[i].fields) {
-            newCentroids[cluster].addField(name, value);
+        for (const auto& [name, value] : data[i].fields) {
+            newCentroids[cluster].addField(name, newCentroids[cluster].getField(name) + value);
         }
         clusterCounts[cluster]++;
     }
-    for (int i = 0; i < k; i++) {
-        for (auto [name, value] : newCentroids[i].fields) {
-            value /= clusterCounts[i];
+    for (int i = 0; i < k; ++i) {
+        if (clusterCounts[i] > 0) {
+            for (const auto& [name, value] : newCentroids[i].fields) {
+                newCentroids[i].fields[name] = value / clusterCounts[i];
+            }
         }
     }
     return newCentroids;
 }
 
-pair<vector<DynamicClass>, vector<string>> readData(string filepath) {
-    vector<DynamicClass> data1;
-    int line_count = 0;
+pair<vector<DynamicClass>, vector<string>> readData(const string& filepath) {
+    vector<DynamicClass> data;
     vector<string> field_names;
     ifstream file(filepath);
+    if (!file.is_open()) {
+        throw runtime_error("Error opening file: " + filepath + '\n');
+    }
     string line;
-    DynamicClass Obj;
+    bool isHeader = true;
     while (getline(file, line)) {
         stringstream ss(line);
         string cell;
-        if (line_count == 0) { // этот блок собирает заголовки
-            while (getline(ss, cell, ',')) {
+        DynamicClass obj;
+        int field_count = 0;
+        while (getline(ss, cell, ',')) {
+            if (isHeader) {
                 field_names.push_back(cell);
-            }
-        }
-        if (line_count == 1) { // этот блок удаляет из вектора заголовки, относящиеся не к скалярным полям
-            int field_count = 0;
-            DynamicClass Obj;
-            while (getline(ss, cell, ',')) {
-                if (!isNumber(cell) || cell == "") {
-                    field_names.erase(field_names.begin() + field_count);
-                    field_count--;
-                } else {
-                    Obj.addField(field_names[field_count], stod(cell));
+            } else {
+                if (cell.empty() || !isNumber(cell)) {
+                    continue;
                 }
-                field_count++;
+                obj.addField(field_names[field_count], stod(cell));
             }
-            data1.push_back(Obj);
+            field_count++;
         }
-        if (line_count >= 2) { // обычный ввод объектов типа DynamicClass в вектор data1
-            int field_count = 0;
-            DynamicClass Obj;
-            while (getline(ss, cell, ',')) {
-                if (cell == "") {
-                    Obj.addField(field_names[field_count], 0.0);
-                } else if (isNumber(cell)) {
-                    Obj.addField(field_names[field_count], stod(cell));
-                } else {
-                    field_count--;
-                }
-                field_count++;
-            }
-            data1.push_back(Obj);
+        if (!isHeader) {
+            data.push_back(obj);
         }
-        line_count++;
-        if (line_count > 2000) {
-            // throw runtime_error("This table has over 2000 lines");
-        }
+        isHeader = false;
     }
-    return make_pair(data1, field_names);
+    return make_pair(data, field_names);
 }
 
-pair<vector<DynamicClass>, vector<int>> k_means(vector<DynamicClass> data1, vector<string> field_names, int k, int maxIterations) {
-    vector<int> clusters;
-    vector<DynamicClass> centroids = initializeCentroids(data1, k);
-    for (int iter = 0; iter < maxIterations; iter++) {
-        clusters = assignClusters(field_names, data1, centroids);
-        vector<DynamicClass> newCentroids = updateCentroids(data1, clusters, k);
-        // тут нужно добавить проверку на сходимость, т.е если эта итерация не принесла значительные изменения, мы заканчиваем
+
+pair<vector<DynamicClass>, vector<int>> k_means(const vector<DynamicClass>& data, const vector<string>& field_names, int k, int maxIterations) {
+    vector<DynamicClass> centroids = initializeCentroids(data, k);
+    vector<int> clusters(data.size());
+    bool converged = false;
+    for (int iter = 0; iter < maxIterations && !converged; ++iter) {
+        clusters = assignClusters(field_names, data, centroids);
+        vector<DynamicClass> newCentroids = updateCentroids(data, clusters, k);
+        converged = true;
+        for (int i = 0; i < k; ++i) {
+            if (euclideanDistance(field_names, centroids[i], newCentroids[i]) > 1e-4) {
+                converged = false;
+                break;
+            }
+        }
         centroids = newCentroids;
     }
     return make_pair(centroids, clusters);
 }
 
+
+double silhouetteScore(const vector<string>& field_names, const vector<DynamicClass>& data, const vector<int>& clusters, const vector<DynamicClass>& centroids) {
+    vector<double> a(data.size()); // computes cohesion
+    vector<double> b(data.size()); // computes separation
+    vector<double> s(data.size()); // computes silhouette score for a particular data point
+
+    // compute a
+    for (size_t i = 0; i < data.size(); ++i) {
+        int cluster = clusters[i];
+        double sum = 0.0;
+        int count = 0;
+        for (size_t j = 0; j < data.size(); ++j) {
+            if (clusters[j] == cluster) {
+                sum += euclideanDistance(field_names, data[i], data[j]);
+                ++count;
+            }
+        }
+        a[i] = sum / count;
+    }
+
+    // compute b
+    for (size_t i = 0; i < data.size(); ++i) {
+        int cluster = clusters[i];
+        double minDist = numeric_limits<double>::max();
+        for (size_t j = 0; j < centroids.size(); ++j) {
+            if (j != cluster) {
+                double dist = 0.0;
+                int count = 0;
+                for (size_t k = 0; k < data.size(); ++k) {
+                    if (clusters[k] == j) {
+                        dist += euclideanDistance(field_names, data[i], data[k]);
+                        ++count;
+                    }
+                }
+                if (count > 0) {
+                    minDist = min(minDist, dist / count);
+                }
+            }
+        }
+        b[i] = minDist;
+    }
+
+    // compute s
+    for (size_t i = 0; i < data.size(); ++i) {
+        s[i] = (b[i] - a[i]) / max(a[i], b[i]);
+    }
+    double overallSilhouetteScore = accumulate(s.begin(), s.end(), 0.0) / s.size();
+    return overallSilhouetteScore;
+}
+
+
 int main() {
-    pair<vector<DynamicClass>, vector<string>> data = readData("data2.csv");
+    pair<vector<DynamicClass>, vector<string>> data = readData("basic1.csv");
     vector<DynamicClass> data1 = data.first;
     vector<string> field_names = data.second;
-    pair<vector<DynamicClass>, vector<int>> result = k_means(data1, field_names, 3, 100);
+    int k = 4;
+    int maxIterations = 100;
+    pair<vector<DynamicClass>, vector<int>> result = k_means(data1, field_names, k, maxIterations);
     vector<DynamicClass> centroids = result.first;
     vector<int> clusters = result.second;
-    for (int i = 0; i < 3; i++) {
+    for (int i = 0; i < k; ++i) {
         cout << "Cluster: " << i << " centroid: ";
-        for (auto field : centroids[i].fields) { // дебаг вывод
+        for (const auto& field : centroids[i].fields) {
             cout << field.first << ": " << field.second << " ";
         }
         cout << '\n';
     }
-
+    double silhouette = silhouetteScore(field_names, data1, clusters, centroids);
+    cout << "Silhouette score: " << silhouette << '\n';
 
     return 0;
 }
